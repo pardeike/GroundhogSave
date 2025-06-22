d0d4949e1c77de832bd513fba671bdb11404254c
Andreas Pardeike <andreas@pardeike.net>
2025-06-22 23:49:54 +0200

    adds project files

---
+++ Source/Dialog_NamePermadeathFile.cs
@@ -0,0 +1,76 @@
+﻿using RimWorld;
+using System;
+using UnityEngine;
+using Verse;
+
+namespace GroundhogSave
+{
+	public class Dialog_NamePermadeathFile : Window
+	{
+		public string curName;
+		public readonly Action<string> onSave;
+
+		static readonly Color commitmentColor = new(1f, 0.3f, 0.35f);
+		static readonly Color reloadAnytimeColor = new(0.3f, 1f, 0.35f);
+
+		static readonly string commitmentModeString = "CommitmentMode".TranslateWithBackup("PermadeathMode");
+		static readonly string reloadModeString = "ReloadAnytimeMode".Translate();
+		static readonly string titleText = "GroundhogIntroduction".Translate(commitmentModeString.Colorize(commitmentColor), reloadModeString.Colorize(reloadAnytimeColor));
+
+		public override Vector2 InitialSize => new(520f, 220f);
+
+		public Dialog_NamePermadeathFile(string curName, Action<string> onSave)
+		{
+			this.curName = curName;
+			this.onSave = onSave;
+			doCloseX = false;
+			doCloseButton = false;
+			forcePause = true;
+			preventCameraMotion = false;
+			preventDrawTutor = true;
+			absorbInputAroundWindow = false;
+			draggable = true;
+			focusWhenOpened = true;
+		}
+
+		public override void OnAcceptKeyPressed()
+		{
+			if (GenText.IsValidFilename(curName))
+			{
+				Find.WindowStack.TryRemove(this, true);
+				onSave?.Invoke(curName);
+				Close();
+			}
+		}
+
+		public override void DoWindowContents(Rect rect)
+		{
+			Text.Font = GameFont.Small;
+
+			if (Event.current.type == EventType.KeyDown && !Find.WindowStack.GetsInput(this))
+			{
+				Event.current.Use();
+				return;
+			}
+
+			var rectLabel = new Rect(0f, 0f, rect.width, rect.height);
+			Widgets.Label(rectLabel, titleText);
+
+			var rectTextField = new Rect(0f, 80f, rect.width / 2f + 70f, 35f);
+			curName = Widgets.TextField(rectTextField, curName, 40, null);
+
+			var rect1 = new Rect(0f, rect.height - 35f, rect.width / 2f - 10f, 35f);
+			GUI.color = reloadAnytimeColor;
+			if (Widgets.ButtonText(rect1, reloadModeString))
+				Close();
+
+			var rect2 = new Rect(rect.width / 2f + 10f, rect.height - 35f, rect.width / 2f - 10f, 35f);
+			var valid = curName.Length > 0 && GenText.IsValidFilename(curName);
+			GUI.color = valid ? commitmentColor : commitmentColor.SaturationChanged(0.5f);
+			if (Widgets.ButtonText(rect2, commitmentModeString, true, true, valid))
+				OnAcceptKeyPressed();
+
+			GUI.color = Color.white;
+		}
+	}
+}
---
+++ Source/Main.cs
@@ -0,0 +1,112 @@
+﻿using Brrainz;
+using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Reflection;
+using Verse;
+
+using static HarmonyLib.Code;
+
+namespace GroundhogSave
+{
+	[StaticConstructorOnStartup]
+	public static class Main
+	{
+		public static string suffixOriginal = " " + "PermadeathModeSaveSuffix".Translate();
+		public static string suffixReplaced = " " + "PermastartModeSaveSuffix".Translate();
+
+		static Main()
+		{
+			var harmony = new Harmony("net.pardeike.harmony.GroundhogSave");
+			harmony.PatchAll();
+			CrossPromotion.Install(76561197973010050);
+		}
+	}
+
+	[HarmonyPatch]
+	static class QuickTestPlayPatches
+	{
+		[HarmonyPatch(typeof(Root_Play), nameof(Root_Play.SetupForQuickTestPlay))]
+		[HarmonyPostfix]
+		static void Root_Play_SetupForQuickTestPlay_Postfix() => Game_InitNewGame_Patch.isQuickTestPlay = true;
+
+		[HarmonyPatch(typeof(ScenarioLister), nameof(ScenarioLister.MarkDirty))]
+		[HarmonyPostfix]
+		static void ScenarioLister_MarkDirty_Postfix() => Game_InitNewGame_Patch.isQuickTestPlay = false;
+	}
+
+	[HarmonyPatch(typeof(GameInfo), nameof(GameInfo.ExposeData))]
+	static class GameInfo_ExposeData_Patch
+	{
+		[HarmonyPriority(Priority.First)]
+		static void Prefix(GameInfo __instance)
+		{
+			var fileName = __instance.permadeathModeUniqueName;
+			if (fileName != null && fileName.EndsWith(Main.suffixReplaced))
+			{
+				fileName = fileName[..^Main.suffixReplaced.Length];
+				__instance.permadeathModeUniqueName = fileName + Main.suffixOriginal;
+			}
+		}
+	}
+
+	[HarmonyPatch(typeof(PermadeathModeUtility), nameof(PermadeathModeUtility.CheckUpdatePermadeathModeUniqueNameOnGameLoad))]
+	static class PermadeathModeUtility_CheckUpdatePermadeathModeUniqueNameOnGameLoad_Patch
+	{
+		static bool Prefix(string filename) => !Current.Game.Info.permadeathMode || !filename.EndsWith(Main.suffixReplaced);
+	}
+
+	[HarmonyPatch(typeof(Game), nameof(Game.InitNewGame))]
+	static class Game_InitNewGame_Patch
+	{
+		public static bool isQuickTestPlay = false;
+
+		static void Postfix()
+		{
+			if (isQuickTestPlay)
+				return;
+
+			var game = Current.Game;
+			if (game == null || game.info == null)
+				return;
+
+			Find.WindowStack.Add(new Dialog_NamePermadeathFile("", fileName =>
+			{
+				if (!fileName.EndsWith(Main.suffixReplaced))
+					fileName += Main.suffixReplaced;
+
+				game.info.permadeathMode = true;
+				game.info.permadeathModeUniqueName = fileName;
+				GameDataSaveLoader.SaveGame(fileName);
+			}));
+		}
+	}
+
+	[HarmonyPatch(typeof(SavedGameLoaderNow), nameof(SavedGameLoaderNow.LoadGameFromSaveFileNow))]
+	static class SavedGameLoaderNow_LoadGameFromSaveFileNow_Patch
+	{
+		static void Postfix()
+		{
+			var filename = Current.Game?.Info?.permadeathModeUniqueName;
+			if (filename != null && filename.EndsWith(Main.suffixReplaced))
+				Current.Game.Info.permadeathModeUniqueName = filename[..^Main.suffixReplaced.Length] + Main.suffixOriginal;
+		}
+	}
+
+	[HarmonyPatch(typeof(StorytellerUI), nameof(StorytellerUI.DrawStorytellerSelectionInterface))]
+	static class StorytellerUI_DrawStorytellerSelectionInterface_Patch
+	{
+		static readonly MethodInfo m_Gap = SymbolExtensions.GetMethodInfo((Listing l) => l.Gap(0f));
+		static readonly MethodInfo m_AnomalyActive = AccessTools.PropertyGetter(typeof(ModsConfig), nameof(ModsConfig.AnomalyActive));
+
+		static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			var matcher = new CodeMatcher(instructions).MatchStartForward([Ldarg_S, Ldc_R4[15f], Callvirt[m_Gap]])
+				.ThrowIfNotMatch("Failed to find 'infoListing.Gap(15f)' in DrawStorytellerSelectionInterface");
+			var startPos = matcher.Pos;
+			matcher = matcher.MatchStartForward([Call[m_AnomalyActive]]);
+			var endPos = matcher.Pos - 1;
+			return matcher.RemoveInstructionsInRange(startPos, endPos).InstructionEnumeration();
+		}
+	}
+}
